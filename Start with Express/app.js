b42fd1496216a6ebb1b08166f2c625f1cd4cead4
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-12-17 11:24:59 +0530

    upto task-3

--- Start with Express/app.js
+++ Start with Express/app.js
@@ -1,18 +1,25 @@
 const http = require('http')
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('In Middleware')
-  next()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add Product</button</form>'
+  )
 })
 
-app.use((req, res, next) => {
-  console.log('In another Middleware')
-  //   res.send('<h1>Hello Express</h1>')
-  res.send({ key1: 'value' })
+app.post('/product', (req, res) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello Express</h1>')
 })
 
 app.listen(3000)
