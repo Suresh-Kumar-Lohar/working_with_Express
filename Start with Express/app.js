faabff932066a5f0bed52e26a4b34f22172961dc
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-12-17 16:27:54 +0530

    upto task-6

--- Start with Express/app.js
+++ Start with Express/app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express')
 
 const app = express()
@@ -12,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/admin', adminRoutes)
 app.use('/shop', shopRoutes)
 
-app.use('/', (req, res, next) => {
-  res.send('<h2>Page not Found</h2>')
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
